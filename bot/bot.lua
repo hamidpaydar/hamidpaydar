661587d0ba1869a6baadc912eb316d9e211c0079
hamidpaydar <emperorh23@gmail.com>
2016-08-22 14:35:02 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {168398326},
+    sudo_users = {225990375},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
